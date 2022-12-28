8e8a2da6c6b3a573eaef87b9e2cf98efe0374ca5
Vera <akane@inbox.ru>
2022-12-28 10:50:20 +0400

    Добавлен метод выборки из массива

--- control_task/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿string[] array1 = new string[6] {"hello", "pos", "12", "world", "NY", "13689"};
-string[] array2 = new string[array1.Length];
-
-
--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[6] {"hello", "pos", "12", "world", "NY", "13689"};
+string[] array2 = new string[array1.Length];
+
